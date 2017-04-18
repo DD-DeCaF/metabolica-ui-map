dd389e906a0f782c7bbd8baa7422ba04db5f08ac
Danny Dannaher <dandann@biosustain.dtu.dk>
2017-04-18 15:13:44 +0200

    Fix: remove unused code from old dialog

--- src/pathwayvis/pathwayvis.component.ts
+++ src/pathwayvis/pathwayvis.component.ts
@@ -1,7 +1,6 @@
 import * as types from './types';
 import * as template from './views/pathwayvis.component.html';
 import './views/pathwayvis.component.scss';
-import * as _ from 'lodash';
 import * as angular from "angular";
 
 
@@ -9,14 +8,11 @@ import * as angular from "angular";
 export class PathwayVisComponentController {
     public shared: types.Shared;
     public showInfo: any;
-    private $mdDialog: ng.material.IDialogService;
-    private dialog: any;
     private $sharing: any;
     private $scope: angular.IScope;
 
 
     constructor($scope: angular.IScope,
-                $mdDialog: ng.material.IDialogService,
                 $sharing
     ) {
         this.$sharing = $sharing;
@@ -29,12 +25,7 @@ export class PathwayVisComponentController {
             sections: {}
         };
 
-        this.$mdDialog = $mdDialog;
-
         this.showInfo = false;
-
-        this.showDialog();
-
     }
 
     public $onInit(){
@@ -46,16 +37,6 @@ export class PathwayVisComponentController {
         this.showInfo = !this.showInfo;
     }
 
-    public showDialog(): void {
-        let dialog = {
-            contentElement: '#map-options-dialog',
-            parent: angular.element(document.getElementsByClassName('container')[0]),
-            clickOutsideToClose: !_.isEmpty(this.shared.map),
-            escapeToClose: !_.isEmpty(this.shared.map)
-        };
-
-        this.$mdDialog.show(dialog);
-    };
 }
 
 export const PathwayVisComponent: angular.IComponentOptions = {
