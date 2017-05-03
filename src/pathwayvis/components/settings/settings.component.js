5a54af8552a278f5e3449aae0f87a7c3c1432a29
Danny Dannaher <dandann@biosustain.dtu.dk>
2017-05-03 11:25:38 +0200

    fix: deletes js file

--- src/pathwayvis/components/settings/settings.component.js
+++ src/pathwayvis/components/settings/settings.component.js
@@ -6,18 +6,21 @@ exports.__esModule = true;
 var template = require("./settings.component.html");
 require("./settings.component.scss");
 var SettingsComponentController = (function () {
-    function SettingsComponentController($mdSidenav, MapOptions) {
+    function SettingsComponentController($mdSidenav, MapOptions, $interval) {
+        this.animationPromis = null;
+        this.animating = false;
         this.mapOptions = MapOptions;
         this.$mdSidenav = $mdSidenav;
+        this.$interval = $interval;
     }
     SettingsComponentController.prototype.disableInfo = function () {
-        return !this.mapOptions.getCurrentMapInfo()['medium'];
+        return !this.mapOptions.getCurrentMapInfo()['medium'] || this.disableForAnimation();
     };
     SettingsComponentController.prototype.disableMapSelector = function () {
-        return !this.mapOptions.getModel();
+        return !this.mapOptions.getModel() || this.disableForAnimation();
     };
     SettingsComponentController.prototype.disableKnockedOutTab = function () {
-        return !this.mapOptions.getCurrentRemovedReactions();
+        return this.mapOptions.getCurrentRemovedReactions().length <= 0 || this.disableForAnimation();
     };
     SettingsComponentController.prototype.toggleRight = function () {
         this.$mdSidenav('right').toggle();
@@ -27,6 +30,38 @@ var SettingsComponentController = (function () {
     };
     SettingsComponentController.prototype.addMapObject = function () {
         this.mapOptions.addMapObject();
+    };
+    SettingsComponentController.prototype.playIcon = function () {
+        if (this.animationPromis) {
+            return 'pause';
+        }
+        return 'play_arrow';
+    };
+    SettingsComponentController.prototype.toggleAnimation = function () {
+        if (this.animationPromis) {
+            this.animating = false;
+            this.$interval.cancel(this.animationPromis);
+            this.animationPromis = null;
+        }
+        else {
+            this.animating = true;
+            this.animationPromis = this.$interval(this.nextMapObject.bind(this), 500);
+        }
+    };
+    SettingsComponentController.prototype.nextMapObject = function () {
+        this.mapOptions.nextMapObject();
+    };
+    SettingsComponentController.prototype.previousMapObject = function () {
+        this.mapOptions.previousMapObject();
+    };
+    SettingsComponentController.prototype.disablePlayBtn = function () {
+        return this.mapOptions.getNumberOfMapObjects() <= 1;
+    };
+    SettingsComponentController.prototype.disableControls = function () {
+        return this.disablePlayBtn() || this.animationPromis;
+    };
+    SettingsComponentController.prototype.disableForAnimation = function () {
+        return !!this.animationPromis;
     };
     return SettingsComponentController;
 }());
