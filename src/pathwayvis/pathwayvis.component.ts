48011b7d63316cdb1f06a2198f896e67b970e53b
Mátyás Fodor <matyas.fodor@gmail.com>
2017-08-31 17:53:16 +0200

    Revert "fix/attempt to fix share experiment"
    This reverts commit 7d651f021fc827fc62a63a4f7ec25ecb6c76b8b0.

--- src/pathwayvis/pathwayvis.component.ts
+++ src/pathwayvis/pathwayvis.component.ts
@@ -2,48 +2,51 @@ import * as types from './types';
 import * as template from './views/pathwayvis.component.html';
 import './views/pathwayvis.component.scss';
 import * as angular from "angular";
-import { MapOptionService } from "./services/mapoption.service";
+import {MapOptionService} from "./services/mapoption.service";
+
+
 
 export class PathwayVisComponentController {
-  public shared: types.Shared;
-  public showInfo: any;
-  private $sharing: any;
-  private $scope: angular.IScope;
-  private mapOptions: MapOptionService;
+    public shared: types.Shared;
+    public showInfo: any;
+    private $sharing: any;
+    private $scope: angular.IScope;
+    private mapOptions: MapOptionService;
 
-  constructor($scope: angular.IScope,
-    $sharing,
-    mapOptions: MapOptionService,
-  ) {
-    this.$sharing = $sharing;
-    this.$scope = $scope;
-    // Init shared scope
-    this.shared = <any>{
-      loading: 0,
-      map: {},
-      model: {},
-      sections: {},
-    };
+    constructor($scope: angular.IScope,
+                $sharing,
+                MapOptions: MapOptionService,
+    ) {
+        this.$sharing = $sharing;
+        this.$scope = $scope;
+        // Init shared scope
+        this.shared = <any>{
+            loading: 0,
+            map: {},
+            model: {},
+            sections: {}
+        };
 
-    this.mapOptions = mapOptions;
+        this.mapOptions = MapOptions;
 
-    this.showInfo = false;
-  }
-
-  public $onInit() {
-    this.mapOptions.init();
-
-    let item = <types.Experiment>this.$sharing.item('experiment');
-    if (item) {
-      this.mapOptions.addExpMapObject();
-      this.mapOptions.removeMapObject(0);
-      this.mapOptions.setExperiment(item);
+        this.showInfo = false;
     }
-  }
+
+    public $onInit(){
+        this.mapOptions.init();
+
+        let item = this.$sharing.item('experiment');
+        if(item){
+            this.mapOptions.addExpMapObject();
+            this.mapOptions.removeMapObject(0);
+            this.mapOptions.setExperiment(item.id);
+        }
+
+    }
 }
 
 export const PathwayVisComponent: angular.IComponentOptions = {
-  controller: PathwayVisComponentController,
-  controllerAs: 'ctrl',
-  template: template.toString()
+    controller: PathwayVisComponentController,
+    controllerAs: 'ctrl',
+    template: template.toString()
 };
