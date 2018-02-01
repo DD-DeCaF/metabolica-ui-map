c679bb45d49327637a558e9485eb83935d7a3646
Ali Kaafarani <ali@kvikshaug.no>
2018-02-01 17:23:09 +0100

    feat: remove MOMA, linear MOMA and ROOM from available methods
    These are not relevant for strain optimization and likely not of
    interest to the user.

--- src/pathwayvis/consts/methods.ts
+++ src/pathwayvis/consts/methods.ts
@@ -5,9 +5,6 @@ export const methods: Method[] = [
   { id: 'pfba', name: 'pFBA' },
   { id: 'fva', name: 'FVA' },
   { id: 'pfba-fva', name: 'pFBA-FVA' },
-  { id: 'moma', name: 'MOMA' },
-  { id: 'lmoma', name: 'lMOMA' },
-  { id: 'room', name: 'ROOM' },
 ];
 
 export const defaultMethod: Method = methods[1];
