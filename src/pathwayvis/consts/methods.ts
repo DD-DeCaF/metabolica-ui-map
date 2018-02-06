2c9d5e3c361e6bd8b5a7fb7be2d4682d733fbaf6
Svetlana Galkina <svegal@biosustain.dtu.dk>
2018-02-06 12:22:48 +0100

    feat: help dialog for methods; remove the methods that are not used; add datacard.component.scss (#131)

--- src/pathwayvis/consts/methods.ts
+++ src/pathwayvis/consts/methods.ts
@@ -1,10 +1,10 @@
 import { Method } from "../types";
 
 export const methods: Method[] = [
-  { id: 'fba', name: 'FBA' },
-  { id: 'pfba', name: 'pFBA' },
-  { id: 'fva', name: 'FVA' },
-  { id: 'pfba-fva', name: 'pFBA-FVA' },
+  { id: 'fba', name: 'Flux Balance Analysis (FBA)' },
+  { id: 'pfba', name: 'parsimonious FBA' },
+  { id: 'fva', name: 'Flux Variability Analysis (FVA)' },
+  { id: 'pfba-fva', name: 'parsimonious FVA' },
 ];
 
 export const defaultMethod: Method = methods[1];
