f54a49625dd0d698266a7a15eee746c1852e4552
Svetlana Galkina <svegal@biosustain.dtu.dk>
2018-02-10 13:44:49 +0100

    chore: icons, tooltips, titles (#133)

--- src/pathwayvis/consts/methods.ts
+++ src/pathwayvis/consts/methods.ts
@@ -2,9 +2,9 @@ import { Method } from "../types";
 
 export const methods: Method[] = [
   { id: 'fba', name: 'Flux Balance Analysis (FBA)' },
-  { id: 'pfba', name: 'parsimonious FBA' },
+  { id: 'pfba', name: 'Parsimonious FBA' },
   { id: 'fva', name: 'Flux Variability Analysis (FVA)' },
-  { id: 'pfba-fva', name: 'parsimonious FVA' },
+  { id: 'pfba-fva', name: 'Parsimonious FVA' },
 ];
 
 export const defaultMethod: Method = methods[1];
