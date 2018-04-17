d27aca22409096b0652c117a493ccbfbca614032
AlbaLopez <alba.lopez.benito@gmail.com>
2018-04-17 16:12:28 +0200

    feat: added scss style for change bounds component.

--- src/pathwayvis/components/reactionpanel/changeBounds/changeBoundsPanel/changeBoundsPanel.component.ts
+++ src/pathwayvis/components/reactionpanel/changeBounds/changeBoundsPanel/changeBoundsPanel.component.ts
@@ -21,6 +21,7 @@ import { ActionsService } from "../../../../services/actions/actions.service";
 import { access } from "fs";
 import { SharedService } from '../../../../services/shared.service';
 import * as template from "./changeBoundsPanel.component.html";
+import "./changeBoundsPanel.component.scss";
 
 const DecafBiggProxy = 'https://api-staging.dd-decaf.eu/bigg/';
 
@@ -34,6 +35,7 @@ class ChangeBoundsPanelController {
   public clickedItem: string;
   public lowerbound: number;
   public upperbound: number;
+  public items: any;
 
   constructor(
     mapOptions: MapOptionService,
@@ -50,6 +52,7 @@ class ChangeBoundsPanelController {
 
     subscription.add(mapOptions.changeReactionsObservable.subscribe((reactions) => {
       this.changedReactions = reactions;
+      this.updateElementSelected();
     }));
 
     $scope.$on('$destroy', () => {
@@ -78,12 +81,25 @@ class ChangeBoundsPanelController {
 
   public clickedItemFunction(item) {
     this.clickedItem = item.item.id;
+    this.lowerbound = item.item.lower_bound;
+    this.upperbound = item.item.upper_bound;
   }
 
   public changedReactionDisplay(item) {
     return item.id;
   }
 
+  public showFunction(item, first) {
+    return this.clickedItem === item.id || (first && !this.clickedItem) || this.items.length === 1;
+  }
+
+  public updateElementSelected() {
+    if (this.changedReactions.length === 1) {
+      this.clickedItem = this.changedReactions[0].id;
+      this.lowerbound = this.changedReactions[0].lower_bound;
+      this.upperbound = this.changedReactions[0].upper_bound;
+    }
+  }
 }
 
 export const ChangeBoundsPanelComponent = {
